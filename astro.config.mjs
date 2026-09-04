360824bd0a96e7af57e63d7c649cbdca3763123e
sjazbseo <sjazbseo.uk@gmail.com>
2026-09-04 11:33:23 -0300

    Remove postinstall script from package.json

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-export default defineConfig({
-  site: 'https://degroteverzorginggids.nl',
-  integrations: [mdx()],
-});
--- src/lib/blog-posts.ts
+++
@@ -1,5 +0,0 @@
-export const POSTS_PER_PAGE = 4;
-
-export function getBlogPageUrl(page: number): string {
-  return page <= 1 ? '/blog/' : `/blog/page/${page}/`;
-}
